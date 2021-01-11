a3aed671cb0c2acbe332227082cf1479179f34d9
Mert <mertdemirok80@gmail.com>
2021-01-11 13:31:23 +0100

    add NotEnoughMoneyInRegisterException Class

---
+++ src/be/intecbrussel/foodshop/exception/NotEnoughMoneyInRegisterException.java
@@ -0,0 +1,24 @@
+package be.intecbrussel.foodshop.exception;
+
+public class NotEnoughMoneyInRegisterException extends Exception{
+    public NotEnoughMoneyInRegisterException() {
+        super();
+    }
+
+    public NotEnoughMoneyInRegisterException(String message) {
+        super(message);
+    }
+
+    public NotEnoughMoneyInRegisterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughMoneyInRegisterException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotEnoughMoneyInRegisterException(String message, Throwable cause, boolean enableSuppression,
+                                                boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
