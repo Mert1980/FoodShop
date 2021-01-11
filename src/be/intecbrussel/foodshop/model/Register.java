fc32b9b4919e75afa958bb921160aba3a2aaed38
Mert <mertdemirok80@gmail.com>
2021-01-11 14:40:02 +0100

    apply NotEnoughMoneyInRegisterException to deductMoney method

--- src/be/intecbrussel/foodshop/model/Register.java
+++ src/be/intecbrussel/foodshop/model/Register.java
@@ -1,5 +1,7 @@
 package be.intecbrussel.foodshop.model;
 
+import be.intecbrussel.foodshop.exception.NotEnoughMoneyInRegisterException;
+
 public class Register {
     private double money;
 
@@ -15,7 +17,12 @@ public class Register {
         this.money += moneyToAdd;
     }
 
-    public void deductMoney(double moneyToDeduct){
+    public void deductMoney(double moneyToDeduct) throws NotEnoughMoneyInRegisterException {
+        if(moneyToDeduct > this.money){
+            throw new NotEnoughMoneyInRegisterException(new StringBuilder().append("There is not enough money in the " +
+                    "register to proceed this ").append("transaction. Register: " + money + "€ | Trying to deduct: " +
+                    "\" + moneyToDeduct + \"€\"").toString());
+        }
         this.money -= moneyToDeduct;
     }
 
