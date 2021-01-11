5a5d134d65ede071c3148433c96ab7d77b59f029
Mert <mertdemirok80@gmail.com>
2021-01-11 14:42:25 +0100

    add toString method into Customer Class

--- src/be/intecbrussel/foodshop/model/Customer.java
+++ src/be/intecbrussel/foodshop/model/Customer.java
@@ -40,4 +40,10 @@ public class Customer {
     public void setMoney(double money) {
         this.money = money;
     }
+
+    @Override
+    public String toString() {
+        return String.format("CUSTOMER   Name: %-10s  ID: %-10s  E-mail: %-15s  Money: %-5.2f€  ",
+                name, ID, email, money);
+    }
 }
