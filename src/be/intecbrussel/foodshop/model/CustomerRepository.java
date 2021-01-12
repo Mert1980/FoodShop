05985a0f8f60010d3ddb2a268777ce31bca7fdd5
Mert <mertdemirok80@gmail.com>
2021-01-12 05:12:06 +0100

    create Register Class and an empty CustomerRepository

---
+++ src/be/intecbrussel/foodshop/model/CustomerRepository.java
@@ -0,0 +1,4 @@
+package be.intecbrussel.foodshop.model;
+
+public class CustomerRepository {
+}
---
+++ src/be/intecbrussel/foodshop/model/FoodShop.java
@@ -0,0 +1,58 @@
+package be.intecbrussel.foodshop.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class FoodShop {
+    private List<Register> registers;
+    private Stock stock;
+    private CustomerRepository customerRepository;
+
+    public FoodShop(List<Register> registers, Stock stock, CustomerRepository customerRepository) {
+        this.registers = registers;
+        this.stock = stock;
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Register> getRegisters() {
+        return registers;
+    }
+
+    public void setRegisters(List<Register> registers) {
+        this.registers = registers;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public CustomerRepository getCustomerRepository() {
+        return customerRepository;
+    }
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Map<Food, Integer> sellFood(Order order, Customer payingCustomer){
+        // remove order from stock
+        order.getFoodItems()
+                .forEach((food, amount) -> stock.removeFromStock(food, amount));
+
+        // calculate the total order price
+        double totalPrice = order.getTotalPrice();
+
+        // update the money of customer
+        double moneyInHand = payingCustomer.getMoney() - totalPrice;
+        payingCustomer.setMoney(moneyInHand);
+
+        // add money to register (In which register will I add money ???
+
+
+        return order.getFoodItems();
+    }
+}
